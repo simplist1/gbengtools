57ccffd60656a9ff3a7d8cf89239dfbddf6e999e
simplist1 <96026176+simplist1@users.noreply.github.com>
2026-06-27 00:58:27 -0400

    Fix Pages OAuth logout redirect cookies

--- functions/api/github/logout.js
+++ functions/api/github/logout.js
@@ -12,12 +12,18 @@ function clearCookie(name, httpOnly = true) {
   return cookie(name, '', { path: '/', httpOnly, secure: true, sameSite: 'Lax', maxAge: 0 });
 }
 
+function redirect(location, cookies = []) {
+  const headers = new Headers({ Location: location });
+  cookies.forEach(value => headers.append('Set-Cookie', value));
+  return new Response(null, { status: 302, headers });
+}
+
 export async function onRequestGet({ request }) {
   const origin = new URL(request.url).origin;
-  const response = Response.redirect(`${origin}/editor.html?code=gbadmin&github=logged_out`, 302);
-  response.headers.append('Set-Cookie', clearCookie('gbgh_state'));
-  response.headers.append('Set-Cookie', clearCookie('gbgh_verifier'));
-  response.headers.append('Set-Cookie', clearCookie('gbgh_token'));
-  response.headers.append('Set-Cookie', clearCookie('gbgh_user', false));
-  return response;
+  return redirect(`${origin}/editor.html?code=gbadmin&github=logged_out`, [
+    clearCookie('gbgh_state'),
+    clearCookie('gbgh_verifier'),
+    clearCookie('gbgh_token'),
+    clearCookie('gbgh_user', false)
+  ]);
 }
